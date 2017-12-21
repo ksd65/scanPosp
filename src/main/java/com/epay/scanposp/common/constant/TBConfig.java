c1a97ce9fa9cad8e5744ed36a1b7820f0d92e808
linxf <linxf@doone.com.cn>
2017-12-21 10:27:25 +0800

    h5支付

---
+++ src/main/java/com/epay/scanposp/common/constant/TBConfig.java
@@ -0,0 +1,56 @@
+package com.epay.scanposp.common.constant;
+
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+
+import com.epay.scanposp.common.utils.EnvironmentUtil;
+/**
+ * 提呗配置
+ * @author linxf
+ *
+ */
+public class TBConfig {
+	public static String rsaPublicKeyFilePath4httpsipay;
+	public static String rsaPublicKeyFilePath;
+	public static String rsaPrivateKeyFilePath;
+	public static String msServerUrl;
+	public static String shopServerUrl;
+	public static String cooperator_t0;
+	public static String cooperator_t1;
+	public static String subAppid;
+	public static String userIDJF;
+	public static String userIDWJF;
+	public static String orderId;
+	public static String jfsignKey;
+	public static String wjfsignKey;
+	public static String bindAccUrl;
+	public static String payUrl;
+	public static String bgUrl;
+	public static String pageUrl;
+	public static String agentId;
+	public static String yhPublicKey;
+	public static String privateKey;
+	public static String jsDomain;
+	static {
+		//生产环境参数
+		ResourceBundle bundle = PropertyResourceBundle.getBundle(EnvironmentUtil.propertyPath + "tb_config");
+		//测试环境参数
+//		ResourceBundle bundle = PropertyResourceBundle.getBundle("mskeytest/ms_config");
+		msServerUrl = bundle.getString("msServerUrl");
+		shopServerUrl = bundle.getString("shopServerUrl");
+		subAppid = bundle.getString("subAppid");
+		userIDJF=bundle.getString("userIDJF");
+		userIDWJF=bundle.getString("userIDWJF");
+		orderId=bundle.getString("orderId");
+		jfsignKey=bundle.getString("jfsignKey");
+		wjfsignKey=bundle.getString("wjfsignKey");
+		bindAccUrl=bundle.getString("bindAccUrl");
+		payUrl=bundle.getString("payUrl");
+		bgUrl=bundle.getString("bgUrl");
+		pageUrl=bundle.getString("pageUrl");
+		agentId=bundle.getString("agentId");
+		yhPublicKey=bundle.getString("yhPublicKey");
+		privateKey=bundle.getString("privateKey");
+		jsDomain=bundle.getString("jsDomain");
+	}
+}
---
+++ src/main/java/com/epay/scanposp/common/utils/tb/SignUtil.java
@@ -0,0 +1,33 @@
+package com.epay.scanposp.common.utils.tb;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ *  生活圈 加签工具类
+ */
+public class SignUtil {
+
+    public  static String createSign(Map map,String secret){
+
+        StringBuffer sf = new StringBuffer();
+        if (map != null && map.keySet().size() > 0) {
+            Iterator iterator = map.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry entry = (Map.Entry<String, String>) iterator.next();
+                sf.append(entry.getKey()+"="+entry.getValue()+"&");
+            }
+        }
+
+        String s = sf.toString();
+        //移除最后一个&，如果是md5加密，还要加上secret
+        String s2 = s.substring(0, s.length() - 1)+secret;
+        String sign = DigestUtils.md5Hex(s2).toString().toUpperCase();
+
+        return sign;
+
+    }
+
+}
