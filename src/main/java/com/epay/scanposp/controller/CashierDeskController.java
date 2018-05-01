3b31f9622ed42387d0108ec440ac49fe29a28bd2
linxf <linxf@doone.com.cn>
2018-05-01 12:51:31 +0800

    扫码

--- src/main/java/com/epay/scanposp/controller/CashierDeskController.java
+++ src/main/java/com/epay/scanposp/controller/CashierDeskController.java
@@ -4722,11 +4722,11 @@ public class CashierDeskController {
 			
 			SysOffice sysOffice = sysOfficeList.get(0);
 			
-		/*	if(!EpaySignUtil.checksign(sysOffice.getPublicKeyRsa(), srcStr.toString(), signStr)){
+			if(!EpaySignUtil.checksign(sysOffice.getPublicKeyRsa(), srcStr.toString(), signStr)){
 				result.put("returnCode", "0004");
 				result.put("returnMsg", "签名校验错误，请检查签名参数是否正确");
 				return signReturn(result);
-			}*/
+			}
 			
 			
 			String reqMsgId=CommonUtil.getOrderCode();
